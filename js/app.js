060a25cc23eefc1e549adb9a01a7d9aff7d6205e
Hanie24 <sailor_121108@hotmail.com>
2018-01-23 08:31:33 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".splash").fadeOut(1500);
+    },3000);
+
+setTimeout(function() {
+    $(".principal").fadeIn(1500);
+},3000);
+});
+
+
+/*var element=document.createElement("p"); 
+var result=document.createTextNode('Total de alumnas activas= '+ activeTrue);
+element.appendChild(result);
+document.getElementById("active").appendChild(element);
+element.classList.add("students");
+}
+*/
---
+++ js/data.js
@@ -0,0 +1,189 @@
+var data = {
+    "Las Comadres": {
+        [
+        "direccion": "Azcapotzalco Calle Heliopolis 13, colonia Recreo 02070 Ciudad de Mexico",
+        "telefono": "5528689586",
+        "especialidad": "Antojitos Mexicanos",
+        "horarios": "08:00 a 19:00",
+        "rango de precios": "desde $20.00",
+        "servicio a domicilio": false,
+        ]
+    },
+
+    "Maju": {
+        [
+        "direccion": "Azcapotzalco calle Heliopolis 57, colonia Claveria 02080 Ciudad de Mexico",
+        "telefono": "5566501107",
+        "especialidad": "comida corrida",
+        "horarios": "09:00 a 20:00",
+        "rango de precios": "desde $50.00",
+        "servicio a domicilio": false,
+        ]
+    },
+    
+    "Rinconcito": {
+        [
+        "direccion": "Azcapotzalco calle Cairo 130, colonia Claveria 02080 Ciudad de Mexico",
+        "telefono": "5570307140",
+        "especialidad": "hamburgesas",
+        "horarios": "14:00 a 23:00",
+        "rango de precios": "desde $39.00",
+        "servicio a domicilio": false,
+        ]
+    }
+    "El carnal": {
+        [
+        "direccion": "Azcapotzalco calle Heliopolis 64, colonia Recreo 02070 Ciudad de Mexico",
+        "telefono": " ",
+        "especialidad": "tacos", 
+        "horarios": "08:00 a 22:00",
+        "rango de precios": "desde $20.00",
+        "servicio a domicilio": false,
+        ]
+    }
+    "Muy Salsas": {
+        [
+        "direccion": "Azcapotzalco calle Heliopolis 14, colonia Recreo 02070 Ciudad de Mexico",
+        "telefono": "5565469459",
+        "especialidad": "tacos",
+        "horarios": "14:00 a 23:00",
+        "rango de precios": "desde $20.00",
+        "servicio a domicilio": false,
+        ]
+    }
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+    "The Beer box" : [
+        "direccion" : "Av. Azcapotzalco 299, colonia Recreo 02070 Ciudad de Mexico,
+        "telefono" : "5565469459",
+        "especialidad" : 
+        "horarios" : "14:00 a 23:00",
+        "rango de precios" :
+        "servicio a domicilio" :
+
+    ]
+}
